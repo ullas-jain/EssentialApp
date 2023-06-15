f5343d9c78842eccc5e136e5cc5d9dcca5a324f8
ullas-jain <jainullas1994@gmail.com>
2023-06-15 12:48:32 +0530

    `FeedLoaderWithFallbackComposite.load` delivers primary feed on prima…
    …ry loader success

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -16,17 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let url = URL(string: "")!
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
-        let feedLoader = RemoteFeedLoader(url: url, client: client)
-        let imageLoader = RemoteFeedImageDataLoader(client: client)
-        
-        let feedViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: feedLoader,
-            imageLoader: imageLoader)
-        
-        window?.rootViewController = feedViewController
+//        let url = URL(string: "")!
+//        let session = URLSession(configuration: .ephemeral)
+//        let client = URLSessionHTTPClient(session: session)
+//        let feedLoader = RemoteFeedLoader(url: url, client: client)
+//        let imageLoader = RemoteFeedImageDataLoader(client: client)
+//
+//        let feedViewController = FeedUIComposer.feedComposedWith(
+//            feedLoader: feedLoader,
+//            imageLoader: imageLoader)
+//
+//        window?.rootViewController = feedViewController
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
---
+++ EssentialAppTests/EssentialAppTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  EssentialAppTests-Bridging-Header.h
+//  EssentialApp
+//
+//  Created by Jain Ullas on 6/15/23.
+//
+
+#ifndef EssentialAppTests_Bridging_Header_h
+#define EssentialAppTests_Bridging_Header_h
+
+
+#endif /* EssentialAppTests_Bridging_Header_h */
