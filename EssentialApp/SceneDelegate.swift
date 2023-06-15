8872b83c067c5c0e1ac2e0e869960a3efe36e00a
ullas-jain <jainullas1994@gmail.com>
2023-06-15 12:33:05 +0530

    Deletes unused test and adds basic setup to SceneDelegate

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,9 +15,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let url = URL(string: "")!
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let feedLoader = RemoteFeedLoader(url: url, client: client)
+        let imageLoader = RemoteFeedImageDataLoader(client: client)
+        
+        let feedViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: feedLoader,
+            imageLoader: imageLoader)
+        
+        window?.rootViewController = feedViewController
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
