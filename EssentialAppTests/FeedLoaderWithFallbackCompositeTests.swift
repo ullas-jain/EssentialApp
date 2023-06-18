b9eb6e5a82e7c7639dbedca346f200c78e5f30fb
ullas-jain <jainullas1994@gmail.com>
2023-06-18 07:38:47 +0530

    Extract uniqueFeed factory helper into a shared scope to remove dup…
    …lication

--- EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -28,8 +28,8 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
-        let primaryLoader = LoaderStub(result: primaryResult)
-        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let primaryLoader = FeedLoaderStub(result: primaryResult)
+        let fallbackLoader = FeedLoaderStub(result: fallbackResult)
         let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
         trackForMemoryLeaks(primaryLoader, file: file, line: line)
         trackForMemoryLeaks(fallbackLoader, file: file, line: line)
@@ -57,21 +57,4 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 
         wait(for: [exp], timeout: 1.0)
     }
-
-    private func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), url: URL(string: "http://any-url.com")!, description: "any", location: "any")]
-    }
-
-    private class LoaderStub: FeedLoader {
-        private let result: FeedLoader.Result
-        
-        init(result: FeedLoader.Result) {
-            self.result = result
-        }
-
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            completion(result)
-        }
-    }
-
 }
